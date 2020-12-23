6cb0d0128c4b9ac479e80a38d8fae21efd9405c4
Celinaldo <celinaldoalvesferre@gmail.com>
2020-12-22 22:57:51 -0300

    termiando a Recycleview

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/ImcActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/ImcActivity.java
@@ -1,10 +1,14 @@
 package co.tiagoaguiar.codelab.myapplication;
 
 import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -24,27 +28,36 @@ public class ImcActivity extends AppCompatActivity {
 
         Button btnSed = findViewById(R.id.btn_imc_send);
 
-        btnSed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!validate()) {
-                    Toast.makeText(ImcActivity.this, "Todos os campos devem ser maiores que zero", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                String sHeight = editHeight.getText().toString();
-                String sWeight = editWeight.getText().toString();
 
-                int height = Integer.parseInt(sHeight);
-                int weight = Integer.parseInt(sWeight);
-
-
-                double result = calculateImc(height, weight);
-
-                int imcResponseId = imcResponse(result);
-
-                Toast.makeText(ImcActivity.this, imcResponseId,Toast.LENGTH_SHORT).show();
-
+        btnSed.setOnClickListener(view -> {
+            if (!validate()) {
+                Toast.makeText(ImcActivity.this, "Todos os campos devem ser maiores que zero", Toast.LENGTH_LONG).show();
+                return;
             }
+            String sHeight = editHeight.getText().toString();
+            String sWeight = editWeight.getText().toString();
+
+            int height = Integer.parseInt(sHeight);
+            int weight = Integer.parseInt(sWeight);
+
+
+            double result = calculateImc(height, weight);
+
+            int imcResponseId = imcResponse(result);
+
+            AlertDialog dialog = new AlertDialog.Builder(ImcActivity.this)
+                    .setTitle(getString(R.string.imc_response, result))
+                    .setMessage(imcResponseId)
+                    .setPositiveButton(android.R.string.ok, (dialog1, which) -> {
+
+                    })
+                    .create();
+
+            dialog.show();
+
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(editWeight.getWindowToken(), 0);
+            imm.hideSoftInputFromWindow(editHeight.getWindowToken(),0);
         });
 
     }
--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -3,28 +3,59 @@ package co.tiagoaguiar.codelab.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-	private View btnImc;
+	private RecyclerView rvMain;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		btnImc.findViewById(R.id.btn_imc);
+		rvMain.findViewById(R.id.mai_rv);
 
 
-		btnImc.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, ImcActivity.class);
-				startActivity(intent);
-			}
-		});
+		// 1-> Definir o comportamento de exibição do layout da recyclerview
+		//mosaic
+		//grid
+		//liner (horizontal | vertical)
+		rvMain.setLayoutManager(new LinearLayoutManager(this));
+		MainAdapter adapter = new MainAdapter();
+		rvMain.setAdapter(adapter);
 
+	}
+
+	private class MainAdapter extends RecyclerView.Adapter<MainViewHolder>{
+
+
+		@NonNull
+		@Override
+		public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			return null;
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+
+		}
+
+		@Override
+		public int getItemCount() {
+			return 0;
+		}
+	}
+
+	private class MainViewHolder extends RecyclerView.ViewHolder{
+
+		public MainViewHolder(@NonNull View itemView) {
+			super(itemView);
+		}
 	}
 }
